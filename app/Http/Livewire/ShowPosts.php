e3c87eb01c6ea8f103f6f3e7a0d761a73578672a
Cristian López Ramirez <cl827209@gmail.com>
2022-03-18 20:23:28 -0600

    listado y busqueda

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -7,9 +7,14 @@ use App\Models\Post;
 
 class ShowPosts extends Component
 {
+    public $search;
+
     public function render(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
     {
-        $posts = Post::all();
+        $posts = Post::where('title', 'like', '%' . $this->search . '%')
+            ->orWhere('content', 'like', '%' . $this->search . '%')
+            ->get();
+
         return view('livewire.show-posts', compact('posts'));
     }
 }
