680eb4185fb5595639a74ec3e45c6f88139c6b20
Cristian López Ramirez <cl827209@gmail.com>
2022-03-28 18:06:34 -0600

    ordenar de forma ascendente y descendente

--- app/Http/Livewire/ShowAssets.php
+++ app/Http/Livewire/ShowAssets.php
@@ -8,13 +8,30 @@ use App\Models\Asset;
 class ShowAssets extends Component
 {
     public $search;
+    public $sort = 'serial_number';
+    public $direction = 'desc';
 
     public function render()
     {
         $assets = Asset::where('serial_number', 'like', '%' . $this->search . '%')
             ->orWhere('brand', 'like', '%' . $this->search . '%')
+            ->orderBy($this->sort, $this->direction)
             ->get();
 
         return view('livewire.show-assets', compact('assets'));
+    }
+
+    public function order($sort)
+    {
+        if ($this->sort == $sort) {
+            if ($this->direction == 'desc') {
+                $this->direction = 'asc';
+            } else {
+                $this->direction = 'desc';
+            }
+        } else {
+            $this->sort = $sort;
+            $this->direction = 'asc';
+        }
     }
 }
--- resources/views/livewire/show-assets.blade.php
+++ resources/views/livewire/show-assets.blade.php
@@ -16,14 +16,50 @@
                     <table class="min-w-full divide-y divide-gray-200">
                         <thead class="bg-gray-50">
                         <tr>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Serial number
+                            <th scope="col"
+                                class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                wire:click="order('serial_number')"
+                            >
+                                Serial
+                                @if($sort == 'serial_number')
+                                    @if($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
+                                @else
+                                    <i class="fa fa-sort float-right mt-1"></i>
+                                @endif
                             </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                wire:click="order('brand')"
+                            >
                                 Brand
+                                @if($sort == 'brand')
+                                    @if($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
+                                @else
+                                    <i class="fa fa-sort float-right mt-1"></i>
+                                @endif
                             </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                wire:click="order('model')"
+                            >
                                 Model
+                                @if($sort == 'model')
+                                    @if($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
+                                @else
+                                    <i class="fa fa-sort float-right mt-1"></i>
+                                @endif
                             </th>
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Type
